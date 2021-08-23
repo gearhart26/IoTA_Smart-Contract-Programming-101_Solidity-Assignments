81e3d4366e9b20aaa2dd4206c27289ac78488bc2
Dustin Gearhart <gearhart26@gmail.com>
2021-08-23 09:57:55 -0700

    Create Ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,17 @@
+// "OWNABLE" BASE CONTRACT //
+
+pragma solidity 0.7.5;
+
+    contract Ownable {
+
+        address internal owner;
+        
+         modifier onlyOwner{
+            require(msg.sender == owner);
+            _;
+        }
+        
+        constructor(){
+            owner = msg.sender;
+        }
+    }

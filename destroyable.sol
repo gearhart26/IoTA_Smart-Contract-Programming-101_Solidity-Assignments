3e0fcde5f81beb6e32c910bf413ab2a7b67f8048
Dustin Gearhart <gearhart26@gmail.com>
2021-08-23 09:59:01 -0700

    Update destroyable.sol

--- destroyable.sol
+++ destroyable.sol
@@ -1,1 +1,12 @@
+// "DESTROYABLE" CHILD CONTRACT //
 
+import "./Ownable.sol";
+    
+pragma solidity 0.7.5;
+
+    contract Destroyable is Ownable {
+        
+        function DESTROY() public onlyOwner {
+            selfdestruct(msg.sender);
+        }
+    }

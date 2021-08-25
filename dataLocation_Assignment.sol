29e39b6f6975d49b6096c3b75606258eec9018e2
Dustin Gearhart <gearhart26@gmail.com>
2021-08-25 10:13:07 -0700

    Update dataLocation_Assignment.sol

--- dataLocation_Assignment.sol
+++ dataLocation_Assignment.sol
@@ -1,1 +1,6 @@
+// DATA LOCATION ASSIGNMENT //
 
+// Find what is wrong with this codes data location settings in the function. 
+// Need to find what is causing the updateBalance function to malfunction
+
+pragma solidity 0.7.5;
